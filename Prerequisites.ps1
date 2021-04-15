68c52e874c6cf25585d72d525413407f94ae0b34
sock3t <phillip_github@sengel.org>
2021-04-16 00:17:35 +0200

    Remove old code + prereqs

---
+++ Prerequisites.ps1
@@ -0,0 +1,45 @@
+function InstallNuGet
+{
+    if ((Get-PackageProvider -Name NuGet).version -lt 2.8.5.201 ) {
+        try {
+            Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$False -Force
+        }
+        catch [Exception]{
+            $_.message 
+            exit
+        }
+    }
+    else {
+        Write-Host "NuGet already installed"
+    }
+}
+function InstallPreRequisites
+{
+    if (Get-Module -ListAvailable -Name SteamPS) {
+        Write-Host "SteamPS already Installed"
+    } 
+    else {
+        InstallNuGet
+        try {
+            Install-Module -Name SteamPS -AllowClobber -Confirm:$False -Force  
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+    }
+}
+function WaitEnd
+{
+    Write-Host "Hit any key to continue or wait for 5 secs..."
+    $counter = 0
+    while(!$Host.UI.RawUI.KeyAvailable -and ($counter++ -lt 5))
+    {
+        Start-Sleep -Milliseconds 100
+    }
+}
+
+# main
+Set-ExecutionPolicy RemoteSigned -Force
+InstallPreRequisites
+WaitEnd
--- TI-Server-Checker.ps1
+++ TI-Server-Checker.ps1
@@ -1,9 +1,18 @@
-# Pre-Requisites:
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name SteamPS -Force
-Set-ExecutionPolicy RemoteSigned -Force
-
 ## Known Servers - this list is handcrafted
+#AU1: 13.211.86.139
+#AU2: 54.253.198.194
+#BR1: 54.94.45.219
+#BR2: 54.207.198.78
+#BR3: 54.207.198.78
+#NA1: 54.67.100.202
+#NA2: 13.57.204.50
+#NA3: 3.101.83.56
+#NA4: 52.53.225.74
+#NA5: 18.144.168.156
+#NA6: 3.101.104.105
+#NA7: 18.144.64.94
+#NA8: 13.56.16.27
+#EU1: 3.250.191.172
 #EU2: 3.250.111.132
 #EU3: 52.48.44.22
 #EU4: 18.203.67.73
@@ -17,104 +26,51 @@ $Servers = "3.250.111.132", "52.48.44.22", "18.203.67.73", "3.249.154.224", "34.
 workflow GetAllServerInfo_w
 {
     $Servers = "3.250.111.132", "52.48.44.22", "18.203.67.73", "3.249.154.224", "34.244.123.102", "34.240.7.84", "54.171.180.126", "3.251.77.114"
-    ForEach -Parallel ($Server in $Servers) {
-        Get-SteamServerInfo -IPAddress $Server -Port 27015 | Select-Object -Property "Players"
+    ForEach -Parallel ($Server in $Servers)
+    {
+        Get-SteamServerInfo -IPAddress $Server -Port 27015 | Where-Object {$_.Players -lt $_.MaxPlayers} | Select-Object -Property "ServerName", "Players", "IPAddress" | % { $_.ServerName = $_.ServerName.Replace("Official Evrima Stress Test - ", ""); $_ }
     }
 }
 
 Function GetAllServerInfo_f
 {
-    ForEach ($Server in $Servers) {
+    ForEach ($Server in $Servers)
+    {
         Get-SteamServerInfo -IPAddress $Server -Port 27015 | Where-Object {$_.Players -lt $_.MaxPlayers} | Select-Object -Property "ServerName", "Players", "IPAddress" | % { $_.ServerName = $_.ServerName.Replace("Official Evrima Stress Test - ", ""); $_ }
     }
 }
-
-Function CreateTasks
+function CheckPreRequisites
 {
-    $global:task2 = { Get-SteamServerInfo -IPAddress '3.250.111.132' -Port 27015 | Select-Object -Property "Players" }
-    $global:task3 = { Get-SteamServerInfo -IPAddress '52.48.44.22' -Port 27015 | Select-Object -Property "Players" }
-    $global:task4 = { Get-SteamServerInfo -IPAddress '18.203.67.73' -Port 27015 | Select-Object -Property "Players" }
-    $global:task5 = { Get-SteamServerInfo -IPAddress '3.249.154.224' -Port 27015 | Select-Object -Property "Players" }
-    $global:task6 = { Get-SteamServerInfo -IPAddress '34.244.123.102' -Port 27015 | Select-Object -Property "Players" }
-    $global:task7 = { Get-SteamServerInfo -IPAddress '34.240.7.84' -Port 27015 | Select-Object -Property "Players" }
-    $global:task8 = { Get-SteamServerInfo -IPAddress '54.171.180.126' -Port 27015 | Select-Object -Property "Players" }
-    $global:task9 = { Get-SteamServerInfo -IPAddress '3.251.77.114' -Port 27015 | Select-Object -Property "Players" }
+    if (Get-Module -ListAvailable -Name SteamPS) {
+        Write-Host "SteamPS already Installed"
+    } 
+    else {
+        $PrereqFile = "$pwd\Prerequisites.ps1"
+        Start-Process -FilePath 'powershell' -Wait -ArgumentList ( '-NoProfile', $PrereqFile ) -verb RunAs        
+    }
 }
 
-Function StartJobs
-{
-    $global:job2 =  Start-Job -ScriptBlock $task2 
-    $global:job3 =  Start-Job -ScriptBlock $task3
-    $global:job4 =  Start-Job -ScriptBlock $task4
-    $global:job5 =  Start-Job -ScriptBlock $task5
-    $global:job6 =  Start-Job -ScriptBlock $task6
-    $global:job7 =  Start-Job -ScriptBlock $task7
-    $global:job8 =  Start-Job -ScriptBlock $task8
-    $global:job9 =  Start-Job -ScriptBlock $task9
-}
+# Pre-Requisites:
+CheckPreRequisites
 
-Function WaitForJobs
-{
-    # wait for the remaining tasks to complete (if not done yet)
-    $null = Wait-Job -Job $job2, $job3, $job4, $job5, $job6, $job7, $job8, $job9
-}
-
-Function GetJobResults
-{
-    # now they are done, get the results
-    $global:result2 = Receive-Job -Job $job2
-    $global:result3 = Receive-Job -Job $job3
-    $global:result4 = Receive-Job -Job $job4
-    $global:result5 = Receive-Job -Job $job5
-    $global:result6 = Receive-Job -Job $job6
-    $global:result7 = Receive-Job -Job $job7
-    $global:result8 = Receive-Job -Job $job8
-    $global:result9 = Receive-Job -Job $job9
-}
-
-Function ClearJobs
-{
-    # discard the jobs
-    Remove-Job -Job $job2, $job3, $job4, $job5, $job6, $job7, $job8, $job9
-}
-
-Function WriteJobResults
-{
-    # now they are done, get the results
-    Write-Host $result2
-    Write-Host $result3
-    Write-Host $result4
-    Write-Host $result5
-    Write-Host $result6
-    Write-Host $result7
-    Write-Host $result8
-    Write-Host $result9
-}
-
+# Main
 DO
 {
-    #$start = Get-Date
-    #GetAllServerInfo_w
-    #$end = Get-Date
-    #Write-Host -ForegroundColor Red ($end - $start).TotalSeconds
-    
     $start = Get-Date
-    $FreeServers = GetAllServerInfo_f
-    cls
+    $FreeServers = GetAllServerInfo_w
+    #cls
     $FreeServers | Format-Table @{ e='*'; width = 25 }
     $end = Get-Date
     Write-Host -ForegroundColor Red ($end - $start).TotalSeconds
     
-    #$start = Get-Date
-    #CreateTasks
-    #StartJobs
-    #WaitForJobs
-    #GetJobResults
-    #ClearJobs
-    #WriteJobResults
-    #$end = Get-Date
-    #Write-Host -ForegroundColor Red ($end - $start).TotalSeconds
+    $start = Get-Date
+    $FreeServers = GetAllServerInfo_f
+    #cls
+    $FreeServers | Format-Table @{ e='*'; width = 25 }
+    $end = Get-Date
+    Write-Host -ForegroundColor Red ($end - $start).TotalSeconds
 
-    #read-host “Press ENTER to continue...”
+    #read-host "Press ENTER to continue..."
+    Write-Host "Hit Ctrl+C to stop this script or just close the window."
     Start-Sleep -m 1000
 } WHILE ($true)
