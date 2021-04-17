7341d2c907bf2b1cd76adb126bb6720881ed684e
sock3t <phillip_github@sengel.org>
2021-04-17 02:15:42 +0200

    Connection check + interactive + readme

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # TI-Server-Checker
-Checks the status of TI Official Evrima Stress Test Servers
+Checks the status of The Isle "Official Evrima Stress Test" Servers aka Update #3 Beta.
 
-It is a Powershell Script that will install depencies if required.
+It is a Powershell Script that queries all currently known "Stress Test" servers and shows only those that currently have a slot free.
 
-The script does not take any paramters atm. It just queries all currently known The Isle Official Evrima Stress Test servers and shows only those that currently have a slot free.
+The script does not take any paramters, but it asks interactively to choose a region for which you want to check servers. So we only query servers which are relevant.
+
+It also has a check that will recognize once you have made a successful connection with a The Isle server and will automatically cease further querying.
+
+Note:
+This tool depends on the PowerShell Module:
+https://github.com/hjorslev/SteamPS
+which in turn requires the "NuGet" PowerShell PackageProvider.
+
+During startup the script will check the dependencies and install these if required. If you run the script as a non admin user then it will kick up the Windows UAC prompt to either confirm or even ask to enter admin credentials (depends on how your Windows system is set up). Once the dependecies are installed it won't ask for that again.
--- TI-Server-Checker.ps1
+++ TI-Server-Checker.ps1
@@ -1,5 +1,5 @@
 <#
-This tool is based on:
+This tool relies on:
 https://github.com/hjorslev/SteamPS
 
 ## Known Servers - this list is handcrafted
@@ -20,7 +20,7 @@ https://github.com/hjorslev/SteamPS
 #EU2: 3.250.111.132
 #EU3: 52.48.44.22
 #EU4: 18.203.67.73
-#EU5: 3.249.154.224
+#EU5: 3.249.154.224 :51717
 #EU6: 34.244.123.102
 #EU7: 34.240.7.84
 #EU8: 54.171.180.126
@@ -82,6 +82,91 @@ function WaitEnd
     }
 }
 
+function Get-NetworkStatistics
+{
+    <#
+    Based on this work:
+    https://lazywinadmin.com/2011/02/how-to-find-running-processes-and-their.html
+    #>
+    $properties = 'Protocol','LocalAddress','LocalPort'
+    $properties += 'RemoteAddress','RemotePort','State','ProcessName','PID'
+
+    netstat -ano | Select-String -Pattern '\s+(UDP)' | ForEach-Object {
+
+        $item = $_.line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries)
+
+        if($item[1] -notmatch '^\[::')
+        {
+            if (($la = $item[1] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6')
+            {
+                $localAddress = $la.IPAddressToString
+                $localPort = $item[1].split('\]:')[-1]
+            }
+            else
+            {
+                $localAddress = $item[1].split(':')[0]
+                $localPort = $item[1].split(':')[-1]
+            }
+
+            if (($ra = $item[2] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6')
+            {
+                $remoteAddress = $ra.IPAddressToString
+                $remotePort = $item[2].split('\]:')[-1]
+            }
+            else
+            {
+                $remoteAddress = $item[2].split(':')[0]
+                $remotePort = $item[2].split(':')[-1]
+            }
+
+            New-Object PSObject -Property @{
+                PID = $item[-1]
+                ProcessName = (Get-Process -Id $item[-1] -ErrorAction SilentlyContinue).Name
+                Protocol = $item[0]
+                LocalAddress = $localAddress
+                LocalPort = $localPort
+                RemoteAddress =$remoteAddress
+                RemotePort = $remotePort
+                State = if($item[0] -eq 'tcp') {$item[3]} else {$null}
+            } | Select-Object -Property $properties
+        }
+    }
+}
+
+function CheckConnection
+{
+    $TrafficHits = 0
+    Do
+    {
+        $port = Get-NetworkStatistics | Where-Object {$_.ProcessName -eq 'steam' -and $_.LocalPort -ge 50000 -and $_.LocalPort -le 65535 } | Select-Object -ExpandProperty LocalPort
+        if (![string]::IsNullOrWhiteSpace($port))
+        {
+            $TrafficHits++
+            if ($TrafficHits -eq 1)
+            {
+                Write-Host
+                Write-Host "#######################################"
+                Write-Host "## Connection attempt in progress... ##"
+                Write-Host "#######################################"
+            }
+            if ($TrafficHits -ge 30)
+            {
+                return $true
+            }
+            Start-Sleep -s 1
+        }
+    } While (![string]::IsNullOrWhiteSpace($port))
+    if ($TrafficHits -gt 0)
+    {
+        Write-Host
+        Write-Host "##########################"
+        Write-Host "## Awwwww... next time! ##"
+        Write-Host "##########################"
+        Start-Sleep -s 7
+    }
+    return $false
+}
+
 function CheckPreRequisites
 {
     if (Get-Module -ListAvailable -Name SteamPS) {
@@ -139,6 +224,7 @@ do
             <#
             # https://devblogs.microsoft.com/powershell/powershell-foreach-object-parallel-feature/
             # ^^ this does only speed up if a lot of servers are queried. But usuallly we want to check only the Official servers for our own region
+            # I keep this for future reference in case the usage of this script ever gets expanded.
             
             $start = Get-Date
             $FreeServers = GetAllServerInfo_w
@@ -162,7 +248,17 @@ do
             #Write-Host -ForegroundColor Red ($end - $start).TotalSeconds
 
             Write-Host "Hit any key to get back to the Region selection."
-            Start-Sleep -m 1000
+            Start-Sleep -m 100
+            if (CheckConnection)
+            {
+                Write-Host
+                Write-Host
+                Write-Host "###############################################################"
+                Write-Host "## Connection to The Isle server detected. Stopping queries. ##"
+                Write-Host "###############################################################"
+                Start-Sleep -s 10
+                break
+            }
         }
         until ($Host.UI.RawUI.KeyAvailable)
     }
